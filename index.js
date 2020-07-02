c859559bca158442a963c56b915a21ccf8324ec8
Edwin Rodriguez Ortiz <rodriguezedwin1324@gmail.com>
2020-07-02 02:38:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function iterativeLog(array){
-  array.forEach(element, index, array){
-    console.log(`${index}: ${element}`)
+  array.forEach((element, index, array) => {
+      console.log(`${index}: ${element}`))
   }
+
 }
